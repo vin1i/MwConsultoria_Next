8a6aa1d306b7c530ef819b97d03fe9093868a165
Vini <126819626+vin1i@users.noreply.github.com>
2025-01-24 15:31:09 -0300

    Corrigindo erro de tipagem do ts

--- src/components/ui/inputs/input.tsx
+++ src/components/ui/inputs/input.tsx
@@ -2,11 +2,12 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  error?: string
-  size?: "sm" | "md" | "lg"
-  label?: string
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
+  error?: string;
+  size?: "sm" | "md" | "lg";
+  label?: string;
 }
+
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, error, size = "md", label, ...props }, ref) => {

57443b4b179b1a1db10e184eb49dfe9781c117f2
Vini <126819626+vin1i@users.noreply.github.com>
2025-02-07 11:40:54 -0300

    fix: warning message when no imóvel is found

--- src/pages/imoveis/index.jsx
+++ src/pages/imoveis/index.jsx
@@ -23,37 +23,37 @@ const ImoveisPage = () => {
 
   const { setIsLoading, isLoading } = useLoading();
   const [currentPage, setCurrentPage] = useState(1);
-const [itemsPerPage, setItemsPerPage] = useState(6);
-  
+  const [itemsPerPage, setItemsPerPage] = useState(6);
+
   useEffect(() => {
     async function fetchImoveis() {
-      setIsLoading(true); 
-  
+      setIsLoading(true);
+
       try {
         const querySnapshot = await getDocs(collection(db, "properties"));
         const listaImoveis = querySnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
-  
+
         setImoveis(listaImoveis);
       } catch (error) {
         console.error("Erro ao buscar imóveis:", error);
       } finally {
-      
+
         setTimeout(() => {
-          setIsLoading(false); 
-        }, 1000); 
+          setIsLoading(false);
+        }, 1000);
       }
     }
 
     fetchImoveis();
-  }, [filters, setIsLoading]); 
-  
+  }, [filters, setIsLoading]);
+
 
   const filteredProperties = useMemo(() => {
     const sortedProperties = imoveis.filter((property) => {
-   
+
       if (filters.tipo) {
         if (
           filters.tipo === "venda" &&
@@ -78,7 +78,7 @@ const [itemsPerPage, setItemsPerPage] = useState(6);
         }
       }
 
-    
+
       if (
         (filters.precoMinimo && property.valorVenda < filters.precoMinimo) ||
         (filters.precoMaximo && property.valorVenda > filters.precoMaximo)
@@ -86,7 +86,7 @@ const [itemsPerPage, setItemsPerPage] = useState(6);
         return false;
       }
 
-   
+
       if (filters.quartos && filters.quartos !== "Qualquer") {
         const quartosFilter = Number(filters.quartos.replace("+", ""));
         const quartosProperty = Number(property.quartos);
@@ -99,7 +99,7 @@ const [itemsPerPage, setItemsPerPage] = useState(6);
         }
       }
 
-     
+
       if (filters.suites && filters.suites !== "Qualquer") {
         const suitesFilter = Number(filters.suites.replace("+", ""));
         const suitesProperty = Number(property.suites);
@@ -112,7 +112,7 @@ const [itemsPerPage, setItemsPerPage] = useState(6);
         }
       }
 
-     
+
       if (filters.banheiros && filters.banheiros !== "Qualquer") {
         const banheirosFilter = Number(filters.banheiros.replace("+", ""));
         const banheirosProperty = Number(property.banheiros);
@@ -125,7 +125,7 @@ const [itemsPerPage, setItemsPerPage] = useState(6);
         }
       }
 
-   
+
       if (filters.vagas && filters.banheiros !== "Qualquer") {
         const vagasFilter = Number(filters.vagas.replace("+", ""));
         const vagasProperty = Number(property.vagas);
@@ -141,10 +141,10 @@ const [itemsPerPage, setItemsPerPage] = useState(6);
       return true;
     })
 
-      
-    .sort((a, b) => a.valorVenda - b.valorVenda);
 
-   
+      .sort((a, b) => a.valorVenda - b.valorVenda);
+
+
     if (filters.ordenacaoVenda === "asc") {
       return sortedProperties.sort((a, b) => a.valorVenda - b.valorVenda);
     }
@@ -164,7 +164,7 @@ const [itemsPerPage, setItemsPerPage] = useState(6);
       return sortedProperties.sort((a, b) => b.vlCondominio - a.vlCondominio);
     }
 
-    return sortedProperties; 
+    return sortedProperties;
   }, [imoveis, filters]);
 
   const totalPages = Math.ceil(filteredProperties.length / itemsPerPage);
@@ -180,97 +180,120 @@ const [itemsPerPage, setItemsPerPage] = useState(6);
         setItemsPerPage(6); // Telas maiores (desktops)
       }
     };
-  
+
     updateItemsPerPage();
-  
-    
+
+
     window.addEventListener("resize", updateItemsPerPage);
-  
-    
+
+
     return () => window.removeEventListener("resize", updateItemsPerPage);
   }, []);
-  
+
   const currentProperties = useMemo(() => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     return filteredProperties.slice(startIndex, startIndex + itemsPerPage);
   }, [filteredProperties, currentPage, itemsPerPage]);
 
-  
+
   return (
 
     <>
-    <Head>
+      <Head>
 
-     
-    <title>MW Consultoria Imobiliária - Encontre o imóvel ideal</title>
-    <meta name="description" content="Bem-vindo à MW Consultoria Imobiliária. Encontre os melhores imóveis para compra e locação." />
-    <meta property="og:title" content="MW Consultoria Imobiliária - Encontre o imóvel ideal" />
-    <meta property="og:description" content="Bem-vindo à MW Consultoria Imobiliária. Encontre os melhores imóveis para compra e locação." />
-    <meta property="og:image" content="https://mwconsultoriaimobiliaria.com.br/default-image.jpg" />
-    <meta property="og:url" content="https://mwconsultoriaimobiliaria.com.br/imoveis" />
-    <meta property="og:type" content="website" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="MW Consultoria Imobiliária - Encontre o imóvel ideal" />
-    <meta name="twitter:description" content="Bem-vindo à MW Consultoria Imobiliária. Encontre os melhores imóveis para compra e locação." />
-    <meta name="twitter:image" content="https://mwconsultoriaimobiliaria.com.br/default-image.jpg" />
-    <meta name="robots" content="index, follow" />
-    <link rel="canonical" href="https://mwconsultoriaimobiliaria.com.br/imoveis" />
-  </Head>
 
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-0 mx-10 my-10 min-h-screen">
+        <title>MW Consultoria Imobiliária - Encontre o imóvel ideal</title>
+        <meta name="description" content="Bem-vindo à MW Consultoria Imobiliária. Encontre os melhores imóveis para compra e locação." />
+        <meta property="og:title" content="MW Consultoria Imobiliária - Encontre o imóvel ideal" />
+        <meta property="og:description" content="Bem-vindo à MW Consultoria Imobiliária. Encontre os melhores imóveis para compra e locação." />
+        <meta property="og:image" content="https://mwconsultoriaimobiliaria.com.br/default-image.jpg" />
+        <meta property="og:url" content="https://mwconsultoriaimobiliaria.com.br/imoveis" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="MW Consultoria Imobiliária - Encontre o imóvel ideal" />
+        <meta name="twitter:description" content="Bem-vindo à MW Consultoria Imobiliária. Encontre os melhores imóveis para compra e locação." />
+        <meta name="twitter:image" content="https://mwconsultoriaimobiliaria.com.br/default-image.jpg" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://mwconsultoriaimobiliaria.com.br/imoveis" />
+      </Head>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-0 mx-10 my-10 min-h-screen">
 
         <Filters filters={filters} onFilterChange={setFilters} filtersOptions={filtersOptions} />
-    
 
-      <div className="col-span-3 flex flex-col gap-4">
-        <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-          {currentProperties.length === 0 ? (
-            <p>Nenhum imóvel encontrado com os filtros aplicados.</p>
-          ) : (
-            currentProperties.map((imovel) => (
-              <RealEstateCard
-                key={imovel.id}
-                id={imovel.id}
-                titulo={imovel.titulo}
-                tipo={imovel.tipo}
-                endereco={imovel.endereco}
-                valorVenda={imovel.valorVenda}
-                valorLocacao={imovel.valorLocacao}
-                vlCondominio={imovel.vlCondominio}
-                vlIptu={imovel.vlIptu}
-                quartos={Number(imovel.quartos)}
-                banheiros={Number(imovel.banheiros)}
-                vagas={Number(imovel.vagas)}
-                metrosQuadrados={Number(imovel.metrosQuadrados)}
-                suites={Number(imovel.suites)}
-                cloudinaryBaseUrl={cloudinaryBaseUrl}
-                imagens={
-                  imovel.imagens?.length > 0
-                    ? imovel.imagens.map((img) =>
+
+        <div className="col-span-3 flex flex-col gap-2">
+          <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+            {currentProperties.length === 0 ? (
+
+<div className="flex flex-col items-center justify-center text-gray-600 w-full col-span-3 mt-20">
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="48"
+  height="48"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+  className="lucide lucide-house mb-4"
+>
+  <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+  <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+</svg>
+<p className="text-lg font-semibold text-center">Nenhum imóvel encontrado</p>
+<p className="text-sm text-gray-500 text-center">Tente ajustar os filtros para encontrar mais opções.</p>
+</div>
+
+
+
+
+            ) : (
+              currentProperties.map((imovel) => (
+                <RealEstateCard
+                  key={imovel.id}
+                  id={imovel.id}
+                  titulo={imovel.titulo}
+                  tipo={imovel.tipo}
+                  endereco={imovel.endereco}
+                  valorVenda={imovel.valorVenda}
+                  valorLocacao={imovel.valorLocacao}
+                  vlCondominio={imovel.vlCondominio}
+                  vlIptu={imovel.vlIptu}
+                  quartos={Number(imovel.quartos)}
+                  banheiros={Number(imovel.banheiros)}
+                  vagas={Number(imovel.vagas)}
+                  metrosQuadrados={Number(imovel.metrosQuadrados)}
+                  suites={Number(imovel.suites)}
+                  cloudinaryBaseUrl={cloudinaryBaseUrl}
+                  imagens={
+                    imovel.imagens?.length > 0
+                      ? imovel.imagens.map((img) =>
                         img.startsWith("http")
                           ? img
                           : `${cloudinaryBaseUrl}/image/upload/${img}`
                       )
-                    : ["https://via.placeholder.com/300x200?text=Imagem+Indisponível"]
-                }
-                descricao={imovel.descricao}
-                disponibilidade={imovel.disponibilidade}
+                      : ["https://via.placeholder.com/300x200?text=Imagem+Indisponível"]
+                  }
+                  descricao={imovel.descricao}
+                  disponibilidade={imovel.disponibilidade}
+                />
+              ))
+            )}
+          </div>
+
+          {totalPages > 1 && (
+            <div className="flex justify-center items-center w-full py-4 mt-8 sm:py-2 sm:mt-4 xs:py-1 xs:text-sm">
+              <Pagination
+                totalPages={totalPages}
+                currentPage={currentPage}
+                onPageChange={(page) => setCurrentPage(page)}
               />
-            ))
+            </div>
           )}
         </div>
-
-        {totalPages > 1 && (
-          <div className="flex justify-center items-center w-full py-4 mt-8 sm:py-2 sm:mt-4 xs:py-1 xs:text-sm">
-            <Pagination
-              totalPages={totalPages}
-              currentPage={currentPage}
-              onPageChange={(page) => setCurrentPage(page)}
-            />
-          </div>
-        )}
       </div>
-    </div>
     </>
   );
 };
